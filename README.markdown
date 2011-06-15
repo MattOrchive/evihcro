529fd7ad5cd043ad3a99ad307e5299d2d4c055af
Matthew Vasseur <matt@orchive.com>
2011-06-15 17:38:20 -0400

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# [Or]chive
+
+This is the actual OrchiveApp
+
+Beginning Phase 1
+
+*Orchive.com*
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrchiveApp::Application.config.secret_token = '13665eebca2ebb6c0f288fe030a134b97451f88f398330a1f84eaaca540df28f0adf78404244f82d907655dd6e0226cfb232e1250293aac56ca53e18d30cecb0'
