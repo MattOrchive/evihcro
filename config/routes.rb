f9cfbd4350565600f35662dc4bb54c4bb4fc6eaa
Matthew Vasseur <matt@orchive.com>
2011-06-16 17:34:55 -0400

    Created Databases (Posts, Comments, Tags)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 OrchiveApp::Application.routes.draw do
+  get "home/index"
+
+  resources :posts do 
+    resources :comments
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ OrchiveApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
