b10293c1f6b1d52764716330ca7a971abd7e0f8a
Matthew Vasseur <mattvasseur@gmail.com>
2011-07-06 13:44:02 -0400

    Updated

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
     post.tags.map { |t| t.name }.join(", ")
   end
   def truncate_content(post, content)
-    output = content.truncate(350, :omission => '', :separator=>' ')
+    output = content.truncate(480, :omission => '', :separator=>' ')
     output += link_to('... See More', post, :id=>'omitText') if content.size > 367
     output.html_safe
   end
