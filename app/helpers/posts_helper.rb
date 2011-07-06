d6dbcab4d1f7f8260ede65358eb497c9b2ce1b5f
Matthew Vasseur <mattvasseur@gmail.com>
2011-07-06 16:15:29 -0400

    Added Timestamp

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,9 +2,11 @@ module PostsHelper
   def join_tags(post)
     post.tags.map { |t| t.name }.join(", ")
   end
+  
   def truncate_content(post, content)
     output = content.truncate(425, :omission => '', :separator=>' ')
     output += link_to('... See More', post, :id=>'omitText') if content.size > 425
     output.html_safe
   end
+  
 end
