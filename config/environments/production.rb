72ed606d324a63fa2829f460843661f1bc18b4b9
Matthew Vasseur <mattvasseur@gmail.com>
2011-07-11 16:37:05 -0400

    Updated app;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ OrchiveApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+	
+	config.action_mailer.default_url_options = { :host => 'http://www.orchive.heroku.com' }
 end
