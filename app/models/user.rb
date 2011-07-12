70cf8c451d969ffc3e038405005f602f36ac846a
Henry Liu <henry@liuhenry.com>
2011-07-12 00:37:23 -0400

    Beta code validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
+  validate :check_beta_code
+
+  def check_beta_code
+    errors.add(:beta_code, "incorrect beta code") if beta_code != 'code'
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :beta_code, :email, :password, :password_confirmation, :remember_me
 end
