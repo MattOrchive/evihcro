1ccc95566e61e8981ea53b2c88445ae70bce0ac3
Jonathan Tang <kyothine@gmail.com>
2011-07-27 20:24:13 -0400

    voting test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -104,6 +104,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1.xml
   def destroy
     @post = Post.find(params[:id])
+    @post.vote_up
     @post.destroy
 
     redirect_to(:action=>'index')
